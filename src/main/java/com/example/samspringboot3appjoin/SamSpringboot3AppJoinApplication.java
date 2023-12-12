a3bc19e657ed336325deb572645c979dd9220d7a
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-12 21:08:42 +0900

    first commit

---
+++ src/main/java/com/example/samspringboot3appjoin/SamSpringboot3AppJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.samspringboot3appjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamSpringboot3AppJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamSpringboot3AppJoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samspringboot3appjoin/SamSpringboot3AppJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samspringboot3appjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamSpringboot3AppJoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
